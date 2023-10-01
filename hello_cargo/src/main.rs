7b13901c9b508cc7d12595cd132c92996031fafc
Nicholas Ferrara <nferrara100@gmail.com>
2023-10-01 15:27:25 +0100

    feat: fib

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn fib (second_last: i32, last: i32, more: i32) -> i32 {
+    let sum = second_last + last;
+    if more <= 0 {
+        return sum
+    }
+    fib(last, sum, more - 1)
+}
+
+fn main () {
+    println!("Result: {}", fib(1, 2, 6))
 }
